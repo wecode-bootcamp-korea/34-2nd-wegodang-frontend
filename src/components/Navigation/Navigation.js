9d76bc083832be51cb9af2a222e4223c656eadc0
shs <hs.seo@kisos.net>
2022-07-05 22:35:46 +0900

    ADD : Navigation 구성 완료

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import styled from 'styled-components';
+import * as S from './Navigation.styled';
 
 const Navigation = props => {
-  return <Nav>Navigation</Nav>;
+  return (
+    <S.Wrapper>
+      <S.Logo>Wadiz</S.Logo>
+      <S.WrapperSearch>
+        <S.SearchBarForm>
+          <S.SearchIcon alt="돋보기" src="/images/icons/search.png" />
+          <S.SearchBar placeholder="어떤 프로젝트를 찾고 계신가요?" />
+        </S.SearchBarForm>
+      </S.WrapperSearch>
+      <S.WrapperUser>
+        <S.UserLink to="/">로그인</S.UserLink>
+        <S.UserLink to="/">회원가입</S.UserLink>
+        <S.ProjectButton>프로젝트 오픈 신청</S.ProjectButton>
+      </S.WrapperUser>
+    </S.Wrapper>
+  );
 };
-
-const Nav = styled.nav`
-  height: 60px;
-  background-color: gray;
-`;
 
 export default Navigation;
---
+++ src/components/Navigation/Navigation.styled.js
@@ -0,0 +1,75 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+export const Wrapper = styled.nav`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  height: 60px;
+`;
+
+export const Logo = styled.p`
+  font-size: 30px;
+  font-family: 'Poppins', sans-serif;
+  flex: 1;
+`;
+
+export const WrapperSearch = styled.div`
+  display: flex;
+  justify-content: center;
+  flex: 1;
+  height: 100%;
+`;
+
+export const SearchBarForm = styled.form`
+  position: relative;
+  border: 1px solid #58c1c2;
+  height: 40px;
+  width: 320px;
+  border-radius: 20px;
+  font-size: 20px;
+  padding: 0 40px;
+`;
+
+export const SearchIcon = styled.img`
+  position: absolute;
+  width: 20px;
+  left: 15px;
+  top: 10px;
+`;
+
+export const SearchBar = styled.input`
+  height: 100%;
+  width: 100%;
+  border: 0;
+  font-size: 20px;
+  &:focus {
+    outline: none;
+  }
+
+  &::placeholder {
+    font-size: 17px;
+  }
+`;
+
+export const WrapperUser = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  flex: 1;
+  height: 100%;
+`;
+
+export const UserLink = styled(Link)`
+  margin: 15px;
+  color: #777b7e;
+`;
+
+export const ProjectButton = styled.button`
+  margin: 15px;
+  padding: 7px 15px;
+  border: 1px solid #58c1c2;
+  border-radius: 4px;
+  background-color: inherit;
+  color: #58c1c2;
+  font-size: 17px;
+`;
