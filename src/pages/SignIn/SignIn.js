23b86bdb71e884f712735a313f0806fe7d0f82f0
chlwogur31 <chlwogur1995@naver.com>
2022-07-08 17:21:22 +0900

    Add: login page 구성 완료 	리뷰 사항 수정 완료

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import * as S from './SignIn.styled';
 
 const SignIn = props => {
-  return <>SignIn</>;
+  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+  const REDIRECT_URI = 'http://localhost:3000/oauth';
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  return (
+    <S.LoginContainer>
+      <S.LoginWrapper>
+        <S.LoginTitle>wegodang</S.LoginTitle>
+        <S.LoginTitle fontSize="60" marginBottom="70">
+          로그인
+        </S.LoginTitle>
+        <S.LoginImg src="/images/SignInImages/btnG_완성형.png" alt="네이버" />
+        <a href={KAKAO_AUTH_URL}>
+          <S.LoginImg
+            src="/images/SignInImages/kakao_login_large_narrow.png"
+            alt="카카오"
+          />
+        </a>
+        <S.LoginImg src="/images/SignInImages/apple_login.png" alt="애플" />
+        <S.LoginDivLink to="/sign-up">회원가입이 필요하신가요?</S.LoginDivLink>
+      </S.LoginWrapper>
+    </S.LoginContainer>
+  );
 };
 
 export default SignIn;
