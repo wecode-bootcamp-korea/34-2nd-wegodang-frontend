449ed5ea468022d5350f3ebdbcc221d6e497eba6
Ahnjungyoung <90163269+Ahnjungyoung@users.noreply.github.com>
2022-07-10 17:30:30 +0900

    [mainpage Slider] slider 통신기능 구현중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,6 +3,7 @@ import ItemCards from './ItemCards/ItemCards';
 import * as S from './Main.styled';
 import useFetch from '../../hooks/useFetch';
 import HttpClient from '../../services/wecode';
+import SimpleSlider from './Components/Slider';
 
 const httpClient = new HttpClient(process.env.REACT_APP_LOCAL_URL);
 
@@ -14,9 +15,9 @@ const Main = () => {
 
   if (loading) return <p>Loadding</p>;
   if (error) return <p>{error}</p>;
-
   return (
     <S.WrapperMain>
+      <SimpleSlider />
       <ItemCards fundItems={payload.products} />
     </S.WrapperMain>
   );
