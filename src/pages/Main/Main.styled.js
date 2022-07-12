e311ebbfa548ecb15319b324a9b1e1d5c777b75d
Ahnjungyoung <90163269+Ahnjungyoung@users.noreply.github.com>
2022-07-12 15:22:05 +0900

    [main Categories]: 스타일 충돌 해결, 통신세팅

--- src/pages/Main/Main.styled.js
+++ src/pages/Main/Main.styled.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const WrapperMain = styled.main`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  width: 100vw;
+  /* align-items: center; */
+  /* width: 100vw; */
 `;
